55acf1272e1728186a20a4dc556bd6ed684d96f2
Jerome Dane <jerome.dane@gmail.com>
2016-06-05 21:43:22 -0400

    Version 0.1.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ev3dev-lang "0.1.1-SNAPSHOT"
+(defproject ev3dev-lang "0.1.1"
   :description "Clojure wrapper around ev3dev API."
   :url "https://github.com/jeromedane/ev3dev-lang-clj"
   :license {:name "Eclipse Public License"
